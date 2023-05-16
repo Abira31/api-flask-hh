fc6154f940d562c0334febacbfac4143abb3318d
arturokb2 <tyktybaev_ad@okb2-tmn.ru>
2023-05-16 10:18:52 +0500

    Добавлен jwt

--- api/__init__.py
+++ api/__init__.py
@@ -4,17 +4,20 @@ from flask_migrate import Migrate
 from flask_restful import Api
 import config
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_jwt_extended import JWTManager
+
 app = Flask(__name__)
 app.config.from_object(config.Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 api = Api(app,prefix='/api/v1')
-SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = '/static/docs/openapi/swagger.yml'  # Our API url (can of course be a local resource)
-# from . models import *
+jwt = JWTManager(app)
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/docs/openapi/swagger.yml'
 
 swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    SWAGGER_URL,
     API_URL
 )
 app.register_blueprint(swaggerui_blueprint)
--- api/routes.py
+++ api/routes.py
@@ -3,9 +3,12 @@ from api.resources.role import RoleAPI
 from api.resources.skills import SkillsAPI
 from api.resources.vacancy import VacancyAPI
 from api.resources.user import UserAPI
+from api.resources.login import LoginAPI,RefreshLoginAPI
 
 
 api.add_resource(RoleAPI,'/role','/role/<id>',strict_slashes=False)
 api.add_resource(SkillsAPI,'/skills','/skills/<id>',strict_slashes=False)
 api.add_resource(VacancyAPI,'/vacancy','/vacancy/<id>',strict_slashes=False)
 api.add_resource(UserAPI,'/user','/user/<id>',strict_slashes=False)
+api.add_resource(LoginAPI,'/login',strict_slashes=False)
+api.add_resource(RefreshLoginAPI,'/refresh',strict_slashes=False)
