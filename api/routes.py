2256073133ab90a1bf5ccc8c52cd088da2cbbae2
arturokb2 <tyktybaev_ad@okb2-tmn.ru>
2023-05-18 15:58:51 +0500

    Добавлен СompanyAPI

--- api/routes.py
+++ api/routes.py
@@ -4,7 +4,7 @@ from api.resources.skills import SkillsAPI
 from api.resources.vacancy import VacancyAPI
 from api.resources.user import UserAPI,UserRoleAPI,UserResumesAPI
 from api.resources.login import LoginAPI,RefreshLoginAPI
-
+from api.resources.company import СompanyAPI
 
 api.add_resource(RoleAPI,'/role','/role/<id>',strict_slashes=False)
 api.add_resource(SkillsAPI,'/skills','/skills/<id>',strict_slashes=False)
@@ -14,3 +14,4 @@ api.add_resource(UserRoleAPI,'/user/role',strict_slashes=False)
 api.add_resource(UserResumesAPI,'/user/resumes','/user/resumes/<id>',strict_slashes=False)
 api.add_resource(LoginAPI,'/jwt/create',strict_slashes=False)
 api.add_resource(RefreshLoginAPI,'/jwt/refresh',strict_slashes=False)
+api.add_resource(СompanyAPI,'/company',strict_slashes=False)
