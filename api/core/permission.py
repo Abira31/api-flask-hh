5df5bcfe46d72a99e1b3df7ad2931ad4dbeb3657
arturokb2 <tyktybaev_ad@okb2-tmn.ru>
2023-05-18 14:14:26 +0500

    Добавлен permission is_user_company
    Только пользователь у которого есть роль соискатель может регистрировать компанию на сайте.

--- api/core/permission.py
+++ api/core/permission.py
@@ -1,7 +1,9 @@
 from functools import wraps
 from flask_jwt_extended import current_user
 from flask import jsonify,make_response,request
-
+from api import db
+from api.models import User,Role
+from sqlalchemy.orm import join
 def is_admin():
     def wrapper(fn):
         @wraps(fn)
@@ -16,5 +18,21 @@ def is_admin():
                 )
                 response.headers["Content-Type"] = "application/json"
                 return response
+        return decorator
+    return wrapper
+
+def is_user_company():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            role_db = db.session.query(Role).select_from(join(Role, User, User.roles))\
+                .filter(Role.name == 'работодатель',User.id == current_user.id).first()
+            if role_db is not None:
+                return fn(*args, **kwargs)
+            response = make_response(
+                jsonify(msg="Не достаточно прав"), 403
+            )
+            response.headers["Content-Type"] = "application/json"
+            return response
         return decorator
     return wrapper
